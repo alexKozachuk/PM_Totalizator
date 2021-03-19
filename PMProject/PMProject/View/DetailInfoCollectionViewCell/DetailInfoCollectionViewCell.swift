d10d85290298b87a5ea341b5f8272595da8eecca
alexKozachuk <grayid@icloud.com>
2021-03-19 17:58:13 +0200

    Add detailFeed View Controller && Extension for work with collectioViews

---
+++ PMProject/PMProject/View/DetailInfoCollectionViewCell/DetailInfoCollectionViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  DetailInfoCollectionViewCell.swift
+//  PMProject
+//
+//  Created by Sasha on 19/03/2021.
+//
+
+import UIKit
+
+class DetailInfoCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var leftValueLabel: UILabel?
+    @IBOutlet weak var rightValueLabel: UILabel?
+    @IBOutlet weak var nameLabel: UILabel?
+    
+
+}
