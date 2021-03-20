23571e2347a19c2db362e1bbf1609715acda90a2
alexKozachuk <grayid@icloud.com>
2021-03-20 14:25:58 +0200

    Add model for event

---
+++ PMProject/PMProject/Model/Event/BetSum.swift
@@ -0,0 +1,14 @@
+//
+//  BetSum.swift
+//  PMProject
+//
+//  Created by Sasha on 20/03/2021.
+//
+
+import Foundation
+
+struct BetSum {
+    var firstBet: Int
+    var secondBet: Int
+    var drawBet: Int
+}
---
+++ PMProject/PMProject/Model/Event/Team.swift
@@ -0,0 +1,15 @@
+//
+//  Team.swift
+//  PMProject
+//
+//  Created by Sasha on 20/03/2021.
+//
+
+import Foundation
+
+struct Team: Identifiable {
+    var id: Int
+    var name: String
+    var imageUrl: URL
+    var characteristics: [String: String]
+}
