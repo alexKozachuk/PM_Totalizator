c91e49820b313e0ad37cae6f0ac72d1899209f1d
alexKozachuk <grayid@icloud.com>
2021-03-24 16:19:01 +0200

    Add CollectionViewCells for Chat

---
+++ PMProject/PMProject/View/ChatViews/MessageCollectionViewCell/MessageCollectionViewCell.swift
@@ -0,0 +1,49 @@
+//
+//  MessageCollectionViewCell.swift
+//  PMProject
+//
+//  Created by Sasha on 24/03/2021.
+//
+
+import UIKit
+
+class MessageCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var messageLabel: UILabel?
+    @IBOutlet weak var titleLabel: UILabel?
+    @IBOutlet weak var avatarImageView: UIImageView?
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
+    var message: String? {
+        get {
+            messageLabel?.text
+        }
+        set {
+            messageLabel?.text = newValue
+        }
+    }
+    
+    var title: String? {
+        get {
+            titleLabel?.text
+        }
+        set {
+            titleLabel?.text = newValue
+        }
+    }
+    
+    var avatar: UIImage? {
+        get {
+            avatarImageView?.image
+        }
+        set {
+            avatarImageView?.image = newValue
+        }
+    }
+    
+
+}
