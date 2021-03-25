e26ecc0b8b7d9c05f1fb4a78624da70c596ee04d
alexKozachuk <grayid@icloud.com>
2021-03-25 12:07:05 +0200

    Integrate Chat

--- PMProject/PMProject/Extension/String+Extension.swift
+++ PMProject/PMProject/Extension/String+Extension.swift
@@ -18,6 +18,15 @@ extension String {
         return date
     }
     
+    var isoTime: Date? {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+        let date = dateFormatter.date(from: self)
+        
+        return date
+    }
+    
 }
 
 
